bb4b20a8ed59a5dacc66ba830989d46496ad6430
ajpolintan <ajpolintan@dons.usfca.edu>
2024-09-24 17:11:45 -0700

    Added summary

--- pandasExercise.py
+++ pandasExercise.py
@@ -4,5 +4,22 @@ import numpy as np
 bc_data = pd.read_csv('breast-cancer.data')
 bc_data.head()
 
+
 classifications  = bc_data['class']
-print(classifications)
+no = classifications.filter(like='no', axis=1)
+yes = classifications.filter(like='recurrence-events', axis=0)
+print(no)
+
+#look 
+counts = classifications.max()
+common = {}
+print("MOST COMMON: " + counts)
+
+
+
+#displays the most common data type
+summary = bc_data.describe(include=[np.object_, pd.Categorical])
+print(summary)
+
+#print(classifications)
+#compare the counts
